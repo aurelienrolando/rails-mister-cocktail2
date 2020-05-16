d465f16120127d62e33eb49bc35cfe4a67b5cd14
Aurelien Rolando <aurelien.rolando@gmail.com>
2020-05-16 22:53:09 +0200

    Add cocktails controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'open-uri'
+require 'json'
+
+file = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+ingredients = JSON.parse(file.read)
+puts "Creating Ingredients"
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create!(name: ingredient["strIngredient1"])
+  puts "ingredient created"
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
