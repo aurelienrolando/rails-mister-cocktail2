f1dd715d03ecebf8b009c9bd6785ef9f1af9d527
Aurelien Rolando <aurelien.rolando@gmail.com>
2020-05-17 23:17:44 +0200

    clear

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,19 @@
+class DosesController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+end
